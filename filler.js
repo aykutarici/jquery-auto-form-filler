3844fc50c9093a11780e4eef9996abf2fd92ea1c
Aykut Arici <arricie@gmail.com>
2021-04-28 18:11:53 +0300

    Update filler.js

--- filler.js
+++ filler.js
@@ -38,6 +38,28 @@ var fillForm = function ()
                     }
                     input.val(result);
                 }
+                    else if (input.attr("type") == "password")
+                {
+                    var result = '';
+                    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                    var characters_special = '@!#$%^&*()_+=-,./:?><;';
+                    var charactersLength = characters.length;
+                    for (var y = 0; y < 4; y++)
+                    {
+                        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+                    }
+                        result += characters_special.charAt(Math.floor(Math.random() * characters_special.length));
+                    for (var y = 0; y < 4; y++)
+                    {
+                        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+                    }
+                    result += characters_special.charAt(Math.floor(Math.random() * characters_special.length));
+                    for (var y = 0; y < 4; y++)
+                    {
+                        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+                    }
+                    input.val(result);
+                }
             }
             var inputs = $("textarea");
             for (var i = 0; i < inputs.length; i++)
